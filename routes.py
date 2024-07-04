9c9490097967b92735f99b9d5c5a596791cbc277
Ben <140560325+BennyCodesStuff@users.noreply.github.com>
2024-07-04 15:01:10 +1200

    answers working

--- routes.py
+++ routes.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 def home():
   return render_template('cheeseFead.html')
 
-@app.route("/questions/<int:id>")
-def questions(id):
+@app.route("/questions/<int:id>/<int:no>/<int:yes>/<int:maybe>")
+def questions(id, no, yes, maybe):
   if id <= 9:
+    no -= 1
+    yes -= 1
+    maybe -= 1
     conn = sqlite3.connect("CheeseFeed.db")
     cursor = conn.cursor()
     cursor.execute("SELECT theQuestion FROM questions WHERE id = ?",(id,))# ? = id
@@ -17,15 +20,71 @@ def questions(id):
     conn.close()
     id += 1
   else:
-    questions = []
-    questions.append("your cheese")
-  return render_template("questions.html", q=questions, idd=id)
+    return render_template("theCHeeseKenews.html", n=no, y=yes, m=maybe)
+  return render_template("questions.html", q=questions, idd=id, n=no, y=yes, m=maybe)
 
+@app.route('/theCHeeseKenews')
+def theCHeeseKenews():
+  return render_template('theCHeeseKenews.html')
 
 @app.route('/login')
 def login():
   return render_template('loginSignUp.html')
 
+
 if __name__ == "__main__":
   app.run(debug=True)
 
+
+
+
+
+
+
+
+
+
+
+#dont go down trust
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route("/ansers/<int:idd>")
+# def answers(idd):
+#   if idd == 10:
+#     conn = sqlite3.connect("CheeseFeed.db")
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT cheese FROM CheesePersonalty WHERE id = ?",(id,10))# ? = id
+#     answers = cursor.fetchone()
+#     conn.close()
+#   else:
+#     answers = []
+#     answers.append("borken")
+#   return render_template("theCHeeseKenews.html", a=answers)
